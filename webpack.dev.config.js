4c4d0b5da756494bdd693e25472046e3f0027701
joelshaw <jshaw@rvadesignsolutions.com>
2018-10-07 14:48:46 -0400

    Add HTMLPlugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/',
+    entry: './src/App.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     devServer: {
         contentBase: './dist'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Joel Shaw Photography'
+        })
+    ]
 }
